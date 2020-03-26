f58af7597ea2c3476cb31fb551b62cad49250ee4
Xavier Jodoin <xavier@jodoin.me>
2020-03-26 08:56:06 -0400

    use attribute to get email

--- web.go
+++ web.go
@@ -190,7 +190,7 @@ func WebHandler(h func(*Web), section string) httprouter.Handle {
 			if token := samlSP.GetAuthorizationToken(r); token != nil {
 				r = r.WithContext(samlsp.WithToken(r.Context(), token))
 
-				email := token.Attributes.Get("Email")
+				email := token.Attributes.Get("emailaddress")
 				if email == "" {
 					Error(w, fmt.Errorf("SAML token missing email"))
 					return
